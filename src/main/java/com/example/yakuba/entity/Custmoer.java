25abb1f981ea7793c1efb7a8d99fe6dc73620b6c
HiromuneMochida <hiromunemochida@gmail.com>
2020-05-12 00:28:11 +0900

    addNewFile

---
+++ src/main/java/com/example/yakuba/entity/Custmoer.java
@@ -0,0 +1,60 @@
+package com.example.yakuba.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "custmoer")
+public class Custmoer {
+	@Id
+	@Column(name = "custmoer_id")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	@Column(name = "name")
+	private String name;
+	@Column(name = "age")
+	private Integer age;
+	@Column(name = "gender")
+	private String gender;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Integer getAge() {
+		return age;
+	}
+
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	@Override
+	public String toString() {
+		return "Custmoer [id=" + id + ", name=" + name + ", age=" + age + ", gender=" + gender + "]";
+	}
+}
