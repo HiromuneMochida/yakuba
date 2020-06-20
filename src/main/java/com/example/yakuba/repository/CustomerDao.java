e1eb252be64550d137aa1d2cd8ab8a1fddf5a7c2
HiromuneMochida <hiromunemochida@gmail.com>
2020-06-21 01:01:51 +0900

    search form

--- src/main/java/com/example/yakuba/repository/CustomerDao.java
+++ src/main/java/com/example/yakuba/repository/CustomerDao.java
@@ -1,9 +1,16 @@
 package com.example.yakuba.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import com.example.yakuba.entity.Customer;
 
+@Repository
 public interface CustomerDao extends JpaRepository<Customer, Long> {
-
+	@Query("select c from Customer c where concat(c.nameSei,c.nameMei) like %:keyword% order by c.id asc")
+	  List<Customer> findCustomers(@Param("keyword") String keyword);
 }
