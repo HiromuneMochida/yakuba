e4c60ebc871d6117f761c5421ba59984ad7147a6
HiromuneMochida <hiromunemochida@gmail.com>
2020-07-30 10:42:18 +0900

    検索

--- src/main/java/com/example/yakuba/repository/CustomerDao.java
+++ src/main/java/com/example/yakuba/repository/CustomerDao.java
@@ -11,7 +11,7 @@ import com.example.yakuba.entity.Customer;
 
 @Repository
 public interface CustomerDao extends JpaRepository<Customer, Long> {
-	/* 顧客キーワード検索 */
+	/* 顧客情報キーワード検索 */
 	@Query("select c from Customer c where c.userName=:userName and concat(c.nameSei,c.nameMei) like %:keyword% order by c.id asc")
 	List<Customer> findCustomers(@Param("keyword") String keyword, @Param("userName") String userName);
 
